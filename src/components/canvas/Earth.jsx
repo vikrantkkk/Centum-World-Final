a127d10758519e9609e1aa71b3fe7dec0d35ac2b
aniimessh <animeshgupta2.002@gmail.com>
2023-07-18 10:29:21 +0530

    zoom disabled of earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -29,6 +29,7 @@ const EarthCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
+          enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
