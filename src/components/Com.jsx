bae502969be15dd8004fdc33bf648a114ca7a03a
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-01-26 19:50:13 +0530

    all white paper brocure done

--- src/components/Com.jsx
+++ src/components/Com.jsx
@@ -5,6 +5,8 @@ import "slick-carousel/slick/slick-theme.css";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { myFounder } from "../constants";
+import brochurePDF from '../assets/pdf/brochure.pdf'
+import { saveAs } from "file-saver";
 
 const TeamSection = ({ title, data, sliderSettings }) => {
   const sliderRef = useRef(null);
@@ -79,6 +81,12 @@ const Founder = () => {
       },
     ],
   };
+
+  const handleDownload = () => {
+    const fileDownload = "brochure.pdf"; 
+    saveAs(brochurePDF, fileDownload);
+  };
+
   return (
     <>
       <div>
@@ -87,17 +95,17 @@ const Founder = () => {
           data={myFounder}
           sliderSettings={sliderSettings}
         />
-            <div class="flex items-center justify-center mt-10">
+        <div class="flex items-center justify-center mt-10">
           <a
-            href="src/assets/JetTradeFX.pdf"
-            download="JetTradeFX Broucher.pdf"
             class="relative px-7 py-4 font-medium text-white group"
           >
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
             <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
             <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
-            <button class="relative">Download Brochure</button>
+            <button class="relative" onClick={ handleDownload }>
+              Download Brochure
+            </button>
           </a>
         </div>
       </div>
--- src/components/Mytechteam.jsx
+++ src/components/Mytechteam.jsx
@@ -5,6 +5,8 @@ import "slick-carousel/slick/slick-theme.css";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { myTechTeam1, myTechTeam2 } from "../constants";
+import brochurePDF from '../assets/pdf/brochure.pdf'
+import { saveAs } from "file-saver";
 
 
 const TeamSection = ({ title, data, sliderSettings }) => {
@@ -80,6 +82,12 @@ const Founder = () => {
       },
     ],
   };
+
+  const handleDownload = () => {
+    const fileDownload = "brochure.pdf"; 
+    saveAs(brochurePDF, fileDownload);
+  };
+
   return (
     <>
       <div className="mt-20 ">
@@ -96,17 +104,17 @@ const Founder = () => {
           data={myTechTeam2}
           sliderSettings={sliderSettings}
         />
-            <div class="flex items-center justify-center mt-10">
+              <div class="flex items-center justify-center mt-10">
           <a
-            href="src/assets/JetTradeFX.pdf"
-            download="JetTradeFX Broucher.pdf"
             class="relative px-7 py-4 font-medium text-white group"
           >
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
             <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
             <span class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
-            <button class="relative">Download Brochure</button>
+            <button class="relative" onClick={ handleDownload }>
+              Download Brochure
+            </button>
           </a>
         </div>
       </div>
